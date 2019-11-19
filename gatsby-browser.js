24995b07519ec8ec4c1e478f5ff49eef71d61bcc
ChristopherBiscardi <chris@christopherbiscardi.com>
2019-11-19 00:32:47 -0800

    hydrate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 const ReactDOM = require("react-dom");
 exports.replaceHydrateFunction = () => {
   return (element, container, callback) => {
-    console.log("rendering!", element);
-    ReactDOM.createRoot(container).render(element, callback);
+    ReactDOM.createRoot(container, {
+      hydrate: true,
+    }).render(element, callback);
   };
 };
